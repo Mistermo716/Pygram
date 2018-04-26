5924416d692b2b310060a8c21f375eea5c5b30ea
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-04-26 00:29:24 -0700

    Comment on picutre post

---
+++ server/code/test2.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+# no need to jsonify anymore of the returns
+# because flask restful takes care of that
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201
+
+
+api.add_resource(Item, '/item/<string:name>')
+
+app.run(port=8080)
